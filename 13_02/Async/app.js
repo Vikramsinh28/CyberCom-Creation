fc312dfa592942509399402b8dc457066ee852bd
Vikramsinh28 <vikramsinhparmar2812@gmail.com>
2023-02-13 18:26:33 +0530

    async

---
+++ 13_02/Async/app.js
@@ -0,0 +1,26 @@
+// running time 15 second
+console.log('start');
+async function func1(){
+
+    setTimeout(() => {
+        console.log('func1')
+    },5000)
+}
+async function func2(){
+
+    setTimeout(() => {
+        console.log('func2')
+    },10000)
+}
+async function func3(){
+
+    setTimeout(() => {
+        console.log('func3')
+    },15000)
+}
+async function main(){
+    await func1();
+    await func2();
+    await func3();
+}
+main();
