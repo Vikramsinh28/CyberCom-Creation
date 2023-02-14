a6b31bde515cccee3944623f8aba6700e7b3655a
Vikramsinh28 <vikramsinhparmar2812@gmail.com>
2023-02-14 09:38:03 +0530

    async await

--- 13_02/Async/app.js
+++ 13_02/Async/app.js
@@ -24,3 +24,28 @@ async function main(){
     await func3();
 }
 main();
+
+async function func1(){
+    
+        setTimeout(async() => {
+            console.log('func1')
+            await func2();
+        },5000)
+}
+
+async function func2(){
+    
+        setTimeout(async() => {
+            console.log('func2')
+            await func3();
+        },10000)
+}
+async function func3(){
+    
+        setTimeout(async() => {
+            console.log('func3')
+        },15000)
+}
+func1();
+
+
